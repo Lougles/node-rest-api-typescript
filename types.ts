61e6e783d26cda67ef19804ab8ea13d3a28dfe9b
vovacelidze <louglescom@gmail.com>
2022-12-01 18:36:06 +0200

    added tuple, unknow and any types

--- types.ts
+++ types.ts
@@ -5,3 +5,17 @@ let empty: null = null;
 let notInitialize: undefined = undefined;
 let callback = (a: number): number => { return 100 + a };
 
+let anything: any = -20;
+anything = 'Text'
+anything = {};
+
+let some:unknown;
+some = 'Text';
+let str: string;
+if(typeof(some) === 'string'){
+  str = some;
+}
+
+let person: [string, number] = ['Max', 21]
+
+
