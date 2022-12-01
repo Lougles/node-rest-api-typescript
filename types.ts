601e0a19e8ca124be23b2902eff2ab6fbeec3c79
vovacelidze <louglescom@gmail.com>
2022-12-01 18:42:41 +0200

    added oneoftypes typescript variables, functions with ts

--- types.ts
+++ types.ts
@@ -19,3 +19,23 @@ if(typeof(some) === 'string'){
 let person: [string, number] = ['Max', 21]
 
 
+enum Status {
+  LOADING,
+  READY
+}
+
+let twotypes : string | number
+
+let oneoftype: 'enable' | 'disable'
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error('Error');
+}
