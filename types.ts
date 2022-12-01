9f23c07b8686508b93d36d29538575570ec9530f
vovacelidze <louglescom@gmail.com>
2022-12-01 18:19:51 +0200

    added variables with types

---
+++ types.ts
@@ -0,0 +1,7 @@
+let age: number = 50;
+let names: string = 'Max';
+let toggle: boolean = true;
+let empty: null = null;
+let notInitialize: undefined = undefined;
+let callback = (a: number): number => { return 100 + a };
+
